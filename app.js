23d2c97308038095f9657e279230a3a20134b088
kearnz <josephkearney14@gmail.com>
2018-05-03 16:12:20 -0500

    udpated after style guide

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const cookieSession = require('cookie-session'),
-	express = require('express'),
-	{ Client } = require('pg'),
-	config = require('./config');
+  express = require('express'),
+  { Client } = require('pg'),
+  config = require('./config');
 
 const app = express(),
-    port = config.port,
-    client = new Client();
+  port = config.port,
+  client = new Client();
 
 // an example of middleware function
 var myLogger = (req, res, next) => {
-    console.log(Date.now());
-    next();
+  console.log(Date.now());
+  next();
 };
 
 // using middleware
 app.use(myLogger);
 
 // example using configured middleware
-app.use(cookieSession({
-	name: 'session',
-	keys: ['key1', 'key2']
-}));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+  })
+);
 
 //demonstrate ability to use postgres with node
 (async () => {
-	await client.connect();
-	const res = await client.query('SELECT $1::text as message', ['Hello World from Postgres!']);
-	console.log(res.rows[0].message); // Hello World ffrom Postgres!
-	await client.end();
+  await client.connect();
+  const res = await client.query('SELECT $1::text as message', ['Hello World from Postgres!']);
+  console.log(res.rows[0].message); // Hello World ffrom Postgres!
+  await client.end();
 })();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-	req.session.views = (req.session.views || 0) + 1;
-	res.end(`hello there, you have ${req.session.views} views`);
+  req.session.views = (req.session.views || 0) + 1;
+  res.end(`hello there, you have ${req.session.views} views`);
 });
 
 //console.log yields error b/c of linter
--- config.js
+++ config.js
@@ -3,11 +3,10 @@ const dotenv = require('dotenv');
 // load the .env file
 dotenv.config();
 
-var fromEnv = (vName) => process.env[vName];
+var fromEnv = vName => process.env[vName];
 
 var config = {
-	port: fromEnv('PORT') || 3000
+  port: fromEnv('PORT') || 3000
 };
 
 module.exports = config;
-
