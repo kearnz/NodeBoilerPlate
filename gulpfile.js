eff3421997a97b57b5f9e001613042756c5ce4c7
kearnz <josephkearney14@gmail.com>
2018-05-12 20:20:50 -0500

    notes update

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,6 @@ gulp.task("default", function() {
 	// getting started with gulp
 	// I prefer to use build scripts, but both can be used
 	// Gulp still allows for js, comments, etc.
+	// and doesn't require bash scripts
 	console.log("gulp vs build scripts???");
 });
