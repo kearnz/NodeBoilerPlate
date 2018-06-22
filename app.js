291d0580471e3dce38349db64c018f2c950d086b
kearnz <josephkearney14@gmail.com>
2018-06-22 11:02:21 -0500

    modularized server stuff

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
 // dependencies we require
 const express = require("express"),
-	cookieSession = require("cookie-session"),
 	config = require("./config/config"),
-	indexRoute = require("./routes/index");
+	serve = require("./server/index");
 
-const app = express(),
-	port = config.port,
-	host = config.host;
-
-// an example of middleware function
-const myDateLogger = (req, res, next) => {
-	console.log(new Date().toISOString());
-	next();
-};
-
-// using middleware for console log (demo only)
-app.use(myDateLogger);
-
-// example using configured middleware
-// note needs to come before indexRoute to pickup session views
-app.use(
-	cookieSession({
-		name: "session",
-		keys: ["key1", "key2"]
-	})
-);
-
-// modularizing routes
-app.use("/", indexRoute);
-
-// console.log yields error b/c of linter
-app.listen(port, host, () =>
-	console.log(`Example app listening on ${host}:${port}!`)
-);
+const app = serve.setup(express());
+serve.start(app, config);
---
+++ server/index.js
@@ -0,0 +1,43 @@
+// dependencies we require
+const cookieSession = require("cookie-session"),
+	bodyParser = require("body-parser"),
+	indexRoute = require("./routes/index");
+
+const setup = server => {
+	// an example of middleware function
+	const myDateLogger = (req, res, next) => {
+		console.log(new Date().toISOString());
+		next();
+	};
+
+	// using middleware for console log (demo only)
+	server.use(myDateLogger);
+
+	// middleware for parsing json
+	server.use(bodyParser.json());
+
+	// example using configured middleware
+	// note needs to come before indexRoute to pickup session views
+	server.use(
+		cookieSession({
+			name: "session",
+			keys: ["key1", "key2"]
+		})
+	);
+
+	// modularizing routes
+	server.use("/", indexRoute);
+};
+// console.log yields error b/c of linter
+const start = (server, config) => {
+	const port = config.port,
+		host = config.host;
+	server.listen(port, host, () =>
+		console.log(`Example app listening on ${host}:${port}!`)
+	);
+};
+
+module.exports = {
+	setup: setup,
+	start: start
+};
